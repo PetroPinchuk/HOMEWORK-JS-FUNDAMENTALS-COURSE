44a41e0359f60a7e6006d1326928232e80f11e3c
Petro <locator862@gmail.com>
2023-03-25 14:13:11 +0100

    Homework 6

---
+++ 6_DOCUMENT OBJECT MODEL (DOM)/main.js
@@ -0,0 +1,11 @@
+
+console.log('%c ---------------Task 1---------------', 'background-color: yellow; font-weight: bold; color:red');
+
+console.log('%c ---------------Task 2---------------', 'background-color: yellow; font-weight: bold; color:red');
+
+console.log('%c ---------------Task 3---------------', 'background-color: yellow; font-weight: bold; color:red');
+
+console.log('%c ---------------Task 4---------------', 'background-color: yellow; font-weight: bold; color:red');
+
+console.log('%c ---------------Task 5---------------', 'background-color: yellow; font-weight: bold; color:red');
+  
