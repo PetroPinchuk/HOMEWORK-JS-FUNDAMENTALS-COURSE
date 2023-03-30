0419d55675da2302d9e711de848e817a15191fab
Petro <locator862@gmail.com>
2023-03-30 16:25:44 +0200

    Task 6

--- 6_DOCUMENT OBJECT MODEL (DOM)/Task_6/main.js
+++ 6_DOCUMENT OBJECT MODEL (DOM)/Task_6/main.js
@@ -1,14 +1,15 @@
 console.log('%c ---------------Task 6---------------', 'background-color: yellow; font-weight: bold; color:red');
 
-let colorButtons = document.querySelectorAll('.color');
-let outPrice = document.querySelector('#outprice');
+const colorButtons = document.querySelector('.colors');
+const outPrice = document.querySelector('#outprice');
 
+colorButtons.addEventListener('click', (e) => changePriceByColor(e));
 
-colorButtons.forEach(button => {
-    button.addEventListener('click', e => {
-        document.querySelector('.active').classList.remove('active');
+function changePriceByColor(e) {
+    document.querySelector('.active').classList.remove('active');
+    const price = Number(e.target.dataset.price);
+    if (e.target.classList.contains('color')) {
         e.target.classList.add('active');
-        const price = Number(e.target.dataset.price);
-        outPrice.innerText = `${price - 1}.99`;
-    })
-})
+        outPrice.innerText = `${(price - 1) + 0.99}$`;
+    }
+}
