b68de19b8fd0cd17219289d743f352cc14400f53
Petro <locator862@gmail.com>
2023-03-31 17:34:13 +0200

    Task 6

--- 6_DOCUMENT OBJECT MODEL (DOM)/Task_6/main.js
+++ 6_DOCUMENT OBJECT MODEL (DOM)/Task_6/main.js
@@ -2,10 +2,13 @@ console.log('%c ---------------Task 6---------------', 'background-color: yellow
 
 const colorButtons = document.querySelector('.colors');
 const sizeButtons = document.querySelector('.sizes');
+const num = document.querySelector('#num');
+let quantity = Number(document.querySelector('#num').value);
 const outPrice = document.querySelector('#outprice');
 
 colorButtons.addEventListener('click', (e) => changePriceByColor(e));
 sizeButtons.addEventListener('click', (e) => changePriceBySize(e));
+num.addEventListener('input', (e) => changePriceByQuntity(e));
 
 function changeShoeImage(e) {
     document.querySelector('.show').classList.remove('show');
@@ -29,11 +32,16 @@ function changePriceBySize(e) {
     }
 }
 
+function changePriceByQuntity(e) {
+    quantity = Number(e.target.value);
+    calculateEndPrice();
+}
+
 function calculateEndPrice() {
     const activeButtons = document.querySelectorAll('.active');
     let activeColorPrice = Number(activeButtons[0].dataset.price);
     let activeSizePrice = Number(activeButtons[1].dataset.price);
-    let endPrice = activeColorPrice + activeSizePrice;
+    let endPrice = (activeColorPrice + activeSizePrice) * quantity;
     outPrice.innerText = `${endPrice - 0.01}$`;
 }
 
