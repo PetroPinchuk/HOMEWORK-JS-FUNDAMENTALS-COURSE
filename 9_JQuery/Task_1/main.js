223eb0783f12497534d3d57a3ea613e9850430e0
Petro <locator862@gmail.com>
2023-04-08 10:25:30 +0200

    Homework 9

---
+++ 9_JQuery/Task_1/main.js
@@ -0,0 +1,2 @@
+
+console.log('%c ---------------Task 1---------------', 'background-color: yellow; font-weight: bold; color:red');
---
+++ 9_JQuery/Task_4/main.js
@@ -0,0 +1,1 @@
+console.log('%c ---------------Task 4---------------', 'background-color: yellow; font-weight: bold; color:red');
